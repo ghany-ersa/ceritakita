e46ed190a9f813a034043e3ad3ef7a97f9cf04c6
Ghany Abdillah Ersa <ghanyersa24@gmail.com>
2026-06-07 23:39:57 +0700

    fix: remove base path configuration for production in Vite setup

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/ceritakita/' : '/',
   plugins: [
     vue(),
     VitePWA({
