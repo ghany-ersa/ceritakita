1aa67c594db1e9d362be8a2513c2e79c208f1a50
Ghany Abdillah Ersa <ghanyersa24@gmail.com>
2026-06-09 01:22:25 +0700

    fix: downgrade Vite and Vue plugin versions for compatibility; update Workbox config

--- vite.config.js
+++ vite.config.js
@@ -56,7 +56,8 @@ export default defineConfig({
         ],
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}']
+        globPatterns: ['**/*.{js,css,html,ico,svg,webp}'],
+        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
       }
     })
   ]
